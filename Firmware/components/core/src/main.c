3b4e9411132f81f744a42e0203e625cc60ef75b5
Michael Shipman <michael@shipman>
2026-04-20 22:07:44 -0400

    remove commented code I'm not going to use

--- Firmware/components/core/src/main.c
+++ Firmware/components/core/src/main.c
@@ -29,21 +29,4 @@ void main(void) {
 
     i++;
   }
-
-  /*
-RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN_Pos);
-
-// do two dummy reads after enabling the peripheral clock, as per the errata
-volatile uint32_t dummy;
-dummy = RCC->AHB1ENR;
-dummy = RCC->AHB1ENR;
-
-GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos);
-
-while (1) {
-GPIOA->ODR ^= (1 << LED_PIN);
-for (uint32_t i = 0; i < 1000000; i++)
-;
-}
-*/
 }
