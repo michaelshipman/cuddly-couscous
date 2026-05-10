530c1c8a84c235e2d335b79e157ade8eab4ec8ca
Michael Shipman <michael@shipman>
2026-05-10 17:24:31 -0400

    got radio HSE32 and FS mode working

--- Firmware/components/core/src/main.c
+++ Firmware/components/core/src/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "radio.h"
 #include "stm32wlxx.h"
 #include "stm32wlxx_ll_dma.h"
 #include "stm32wlxx_ll_dmamux.h"
@@ -16,14 +17,6 @@ char test_str[] = "hello world!\r\n";
 char another_str[] = "a much longer string for testing purposes\r\n";
 char buffer[1024] = "";
 
-__attribute__((optimize("-O0"))) int foo(int j) {
-
-  for (int i = 0; i < j; i++)
-    buffer[0] = i;
-
-  return j;
-}
-
 int main(void) {
   /* System interrupt init*/
   NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
@@ -36,51 +29,13 @@ int main(void) {
 
   lpuart1_init();
 
-  LL_APB3_GRP1_EnableClock(LL_APB3_GRP1_PERIPH_SUBGHZSPI);
+  init_subghz();
 
-  /* Disable SUBGHZSPI Peripheral */
-  CLEAR_BIT(SUBGHZSPI->CR1, SPI_CR1_SPE);
+  set_radio_tcxo();
 
-  /*----------------------- SPI CR1 Configuration ----------------------------*
-   *             SPI Mode: Master                                             *
-   *   Communication Mode: 2 lines (Full-Duplex)                              *
-   *       Clock polarity: Low                                                *
-   *                phase: 1st Edge                                           *
-   *       NSS management: Internal (Done with External bit inside PWR        *
-   *  Communication speed: BaudratePrescaler                             *
-   *            First bit: MSB                                                *
-   *      CRC calculation: Disable                                            *
-   *--------------------------------------------------------------------------*/
-  WRITE_REG(SUBGHZSPI->CR1,
-            (SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_BR_1 | SPI_CR1_SSM));
+  calibrate_radio();
 
-  /*----------------------- SPI CR2 Configuration ----------------------------*
-   *            Data Size: 8bits                                              *
-   *              TI Mode: Disable                                            *
-   *            NSS Pulse: Disable                                            *
-   *    Rx FIFO Threshold: 8bits                                              *
-   *--------------------------------------------------------------------------*/
-  WRITE_REG(SUBGHZSPI->CR2,
-            (SPI_CR2_FRXTH | SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2));
-
-  /* Enable SUBGHZSPI Peripheral */
-  SET_BIT(SUBGHZSPI->CR1, SPI_CR1_SPE);
-
-  LL_RCC_RF_DisableReset();
-  LL_PWR_UnselectSUBGHZSPI_NSS();
-
-  while (LL_RCC_IsRFUnderReset())
-    ;
-
-  LL_PWR_SelectSUBGHZSPI_NSS();
-
-  while (LL_PWR_IsActiveFlag_RFBUSYMS())
-    ;
-
-  LL_PWR_UnselectSUBGHZSPI_NSS();
-
-  while (LL_PWR_IsActiveFlag_RFBUSYMS())
-    ;
+  set_radio_fs();
 
   while (1) {
     while (send_lpuart1_data(test_str, sizeof(test_str)) < 0)
@@ -89,23 +44,9 @@ int main(void) {
     while (send_lpuart1_data(another_str, sizeof(another_str)) < 0)
       ;
 
-    while (LL_PWR_IsActiveFlag_RFBUSY())
-      ;
+    get_radio_status();
 
-    LL_PWR_SelectSUBGHZSPI_NSS();
-
-    while (!LL_PWR_IsSUBGHZSPI_NSS_Selected())
-      ;
-
-    LL_SPI_TransmitData8(SUBGHZSPI, 0xC0);
-
-    uint8_t rx = LL_SPI_ReceiveData8(SUBGHZSPI);
-
-    foo(10);
-
-    LL_PWR_UnselectSUBGHZSPI_NSS();
-
-    snprintf(buffer, sizeof(buffer), "%x\r\n", rx);
+    snprintf(buffer, sizeof(buffer), "%x\r\n", radio.status);
 
     while (send_lpuart1_data(buffer, sizeof(buffer)) < 0)
       ;
--- Firmware/components/peripheral/inc/radio.h
+++ Firmware/components/peripheral/inc/radio.h
@@ -3,10 +3,156 @@
 
 #include "stm32wlxx_ll_pwr.h"
 #include "stm32wlxx_ll_rcc.h"
+#include "stm32wlxx_ll_spi.h"
 #include "stm32wlxx_ll_system.h"
 #include "stm32wlxx_ll_utils.h"
-#include "subghz.h"
 
-static inline void init_subghz(void) {}
+typedef struct {
+  uint8_t status;
+} Radio_t;
+
+Radio_t radio;
+
+__attribute__((optimize("-O0"))) int foo(int j) {
+
+  for (int i = 0; i < j; i++)
+    ;
+
+  return j;
+}
+
+static inline void init_subghz(void) {
+
+  LL_APB3_GRP1_EnableClock(LL_APB3_GRP1_PERIPH_SUBGHZSPI);
+
+  /* Disable SUBGHZSPI Peripheral */
+  CLEAR_BIT(SUBGHZSPI->CR1, SPI_CR1_SPE);
+
+  /*----------------------- SPI CR1 Configuration ----------------------------*
+   *             SPI Mode: Master                                             *
+   *   Communication Mode: 2 lines (Full-Duplex)                              *
+   *       Clock polarity: Low                                                *
+   *                phase: 1st Edge                                           *
+   *       NSS management: Internal (Done with External bit inside PWR        *
+   *  Communication speed: BaudratePrescaler                             *
+   *            First bit: MSB                                                *
+   *      CRC calculation: Disable                                            *
+   *--------------------------------------------------------------------------*/
+  WRITE_REG(SUBGHZSPI->CR1,
+            (SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_BR_1 | SPI_CR1_SSM));
+
+  /*----------------------- SPI CR2 Configuration ----------------------------*
+   *            Data Size: 8bits                                              *
+   *              TI Mode: Disable                                            *
+   *            NSS Pulse: Disable                                            *
+   *    Rx FIFO Threshold: 8bits                                              *
+   *--------------------------------------------------------------------------*/
+  WRITE_REG(SUBGHZSPI->CR2,
+            (SPI_CR2_FRXTH | SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2));
+
+  /* Enable SUBGHZSPI Peripheral */
+  SET_BIT(SUBGHZSPI->CR1, SPI_CR1_SPE);
+
+  LL_RCC_RF_DisableReset();
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+
+  while (LL_PWR_IsActiveFlag_RFBUSY())
+    ;
+
+  while (LL_RCC_IsRFUnderReset())
+    ;
+
+  LL_PWR_SelectSUBGHZSPI_NSS();
+
+  while (LL_PWR_IsActiveFlag_RFBUSYMS())
+    ;
+
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+
+  while (LL_PWR_IsActiveFlag_RFBUSYMS())
+    ;
+}
+
+static inline void set_radio_tcxo(void) {
+
+  while (LL_PWR_IsActiveFlag_RFBUSY())
+    ;
+
+  LL_PWR_SelectSUBGHZSPI_NSS();
+
+  while (!LL_PWR_IsSUBGHZSPI_NSS_Selected())
+    ;
+
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x97);
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x01);
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x00);
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x00);
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x00);
+
+  foo(10);
+
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+
+  LL_RCC_HSE_EnableTcxo();
+
+  LL_RCC_HSE_Enable();
+
+  while (!LL_RCC_HSE_IsReady())
+    ;
+}
+
+static inline void get_radio_status(void) {
+
+  while (LL_PWR_IsActiveFlag_RFBUSY())
+    ;
+
+  LL_PWR_SelectSUBGHZSPI_NSS();
+
+  while (!LL_PWR_IsSUBGHZSPI_NSS_Selected())
+    ;
+
+  LL_SPI_TransmitData8(SUBGHZSPI, 0xC0);
+
+  radio.status = LL_SPI_ReceiveData8(SUBGHZSPI);
+
+  foo(10);
+
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+}
+
+static inline void calibrate_radio(void) {
+
+  while (LL_PWR_IsActiveFlag_RFBUSY())
+    ;
+
+  LL_PWR_SelectSUBGHZSPI_NSS();
+
+  while (!LL_PWR_IsSUBGHZSPI_NSS_Selected())
+    ;
+
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x89);
+  LL_SPI_TransmitData8(SUBGHZSPI, 0x7F);
+
+  foo(10);
+
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+}
+
+static inline void set_radio_fs(void) {
+
+  while (LL_PWR_IsActiveFlag_RFBUSY())
+    ;
+
+  LL_PWR_SelectSUBGHZSPI_NSS();
+
+  while (!LL_PWR_IsSUBGHZSPI_NSS_Selected())
+    ;
+
+  LL_SPI_TransmitData8(SUBGHZSPI, 0xC1);
+
+  foo(10);
+
+  LL_PWR_UnselectSUBGHZSPI_NSS();
+}
 
 #endif
